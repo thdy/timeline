f5c99d36862cac87c9cb75694556ee45f949bfe5
Shintaro Kaku <kaku@hey.jp>
2021-08-29 16:52:40 +0900

    source: Change to thdy's content

--- site.config.ts
+++ site.config.ts
@@ -1,14 +1,13 @@
 export default {
-  title: "catnose's timeline",
-  description: 'personal website by catnose.',
+  title: "thdy's timeline",
+  description: 'personal website by thdy.',
   siteRoot:
     process.env.NODE_ENV === 'production'
-      ? 'https://times.catnose99.com'
+      ? 'https://thdy.jp'
       : 'http://localhost:3000',
   rssUrlList: [
-    'https://zenn.dev/catnose99/feed',
-    'https://medium.com/feed/@catnose99',
-    'https://note.com/catnose/rss',
-    'https://catknows.vercel.app/rss.xml',
+    'https://zenn.dev/thdy/feed',
+    'https://note.com/thdy/rss',
+    'https://thdy.hatenablog.jp/rss',
   ],
 };
--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -5,13 +5,12 @@ export const HomeHero = () => {
   return (
     <div className={styles.container}>
       <img src="/icon.svg" alt="Hello" width={51} height={58} />
-      <h1 className={styles.title}>Hi, I'm catnose</h1>
+      <h1 className={styles.title}>Hi, I'm Shintaro Kaku (@thdy)</h1>
       <p className={styles.description}>
-        Designer, developer, maker, dog &amp; cat lover. Currently working on{' '}
-        <a href="https://zenn.dev">zenn.dev</a> with{' '}
-        <a href="https://classmethod.jp">Classmethod</a>. Follow{' '}
-        <a href="https://twitter.com/catnose99">
-          @catnose99
+        Corporate Engineer. Currently working on{' '}
+        <a href="https://hey.jp">hey, Inc.</a> <br></br>Follow{' '}
+        <a href="https://twitter.com/thdy_jp">
+          @thdy_jp
           <TwitterIcon width={18} height={18} />
         </a>{' '}
         for daily updates.
