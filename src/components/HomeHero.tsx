69dfccaecbab3f75942384175bdf6a54f23bbe49
Shintaro Kaku <thdy00@gmail.com>
2021-09-16 22:58:47 +0900

    chore: change title

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -5,9 +5,9 @@ export const HomeHero = () => {
   return (
     <div className={styles.container}>
       <img src="/icon.svg" alt="Hello" width={51} height={58} />
-      <h1 className={styles.title}>Hi, I'm Shintaro Kaku</h1>
-      <h1 >(a.k.a thdy)</h1>
       <p className={styles.description}>
+        <h2 >Hi there 👋</h2>
+        <h2 >I'm Shintaro Kaku (a.k.a thdy)</h2>
         Corporate Engineer. Currently working on{' '}
         <a href="https://hey.jp">hey, Inc.</a> <br></br>Follow{' '}
         <a href="https://twitter.com/thdy_jp">
