6d9cfcca87b9f5691415de8907303a5434e6ea08
thdy <mail@thdy.jp>
2023-07-10 01:32:44 +0900

    chore: update react version

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -5,13 +5,17 @@ export const HomeHero = () => {
   return (
     <div className={styles.container}>
       <img src="/icon.svg" alt="Hello" width={51} height={51} />
-      <p className={styles.description}>
-        <h2 >Hi there 👋</h2>
-        <h2 >I'm Shintaro Kaku (a.k.a thdy)</h2>
-        Corporate Engineer at{' '}
-        <a href="https://st.inc">STORES, Inc.</a> <br></br>
-        Go to <a href="https://thdy.bio.link/">thdy.bio.link</a> for my profile and various links.
-      </p>
+      <div className={styles.description}>
+        <h2>Hi there 👋</h2>
+        <h2>I'm Shintaro Kaku (a.k.a thdy)</h2>
+        <p>
+          Corporate Engineer at{' '}
+          <a href="https://st.inc">STORES, Inc.</a>
+        </p>
+        <p>
+          Go to <a href="https://thdy.bio.link/">thdy.bio.link</a> for my profile and various links.
+        </p>
+      </div>
     </div>
   );
 };
