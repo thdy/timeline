9dafdc0b2e1662ee80f09ed3c134e48e2bb26f8a
thdy <mail@thdy.jp>
2024-08-26 00:00:46 +0900

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-// import { SiteFooter } from '@src/components/SiteFooter';
-// import { SiteHeader } from '@src/components/SiteHeader';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import siteConfig from '../../site.config';
 import { SiteFooter } from '../components/SiteFooter';
 import '../styles/global/index.scss';
@@ -24,6 +23,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
       <SiteFooter />
     </>
