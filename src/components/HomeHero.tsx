ff9173c4a68007698bd8e56d1bc1a08d9a000723
thdy <thdy00@gmail.com>
2026-05-30 23:58:57 +0900

    fix

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -9,13 +9,10 @@ export const HomeHero = () => {
         <h1>About</h1>
         <h3>Shintaro Kaku / 加来 慎太郎</h3>
         <p>
-          - Corporate Engineer at <a href="https://goinc.jp/">GO Inc.</a>
+          - Corporate Engineer
         </p>
         <p>
           - Remote worker in Hiroshima, Japan
-        </p>
-        <p>
-          - <a href="https://okta.connpass.com/">Japan Okta User Group</a> organizer
         </p>
         <br></br>
         <h1>Links</h1>
@@ -39,15 +36,6 @@ export const HomeHero = () => {
         </p>
         <p>
           <a href="https://lapras.com/public/thdy">LAPRAS</a>
-        </p>
-        <p>
-          <a href="https://www.wantedly.com/id/thdy">Wantedly</a>
-        </p>
-        <p>
-          <a href="https://www.buymeacoffee.com/thdy">Buy Me a Coffee</a>
-        </p>
-        <p>
-          <a href="https://www.amazon.jp/hz/wishlist/ls/14DHJZS00NLUS?ref_=wl_share">Amazon wishlist</a>
         </p>
       </div>
     </div>
